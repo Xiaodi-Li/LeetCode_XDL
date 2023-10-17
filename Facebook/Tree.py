2dfe98378f46c5cb8870c9cd0180d54f0e572b96
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-17 15:43:00 -0700

    Update Tree.py

--- Facebook/Tree.py
+++ Facebook/Tree.py
@@ -22,3 +22,20 @@ class Range_Sum_of_BST:
                     stack.append(node.right)
 
         return ans
+
+# 236. Lowest Common Ancestor of a Binary Tree
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+#
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Lowest_Common_Ancestor_of_BT:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root in (None, p, q): return root
+        left, right = (self.lowestCommonAncestor(kid, p, q) for kid in (root.left, root.right))
+        return root if left and right else left or right
