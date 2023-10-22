65fb2cc4f4ef3dbe19c5a38973db3a0972229ac1
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-21 18:46:43 -0700

    Update BFS.py

--- Facebook/BFS.py
+++ Facebook/BFS.py
@@ -33,4 +33,41 @@ class Shortest_Path_in_Binary_Matrix:
 
         return dp[m - 1][n - 1] if dp[m - 1][n - 1] != float('inf') else -1
 
+# 987. Vertical Order Traversal of a Binary Tree
+# Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+#
+# For each node at position (row, col), its left and right children will be at positions (row + 1, col - 1) and (row + 1, col + 1) respectively. The root of the tree is at (0, 0).
+#
+# The vertical order traversal of a binary tree is a list of top-to-bottom orderings for each column index starting from the leftmost column and ending on the rightmost column. There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values.
+#
+# Return the vertical order traversal of the binary tree.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Vertical_Order_Traversal:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        node_list = []
 
+        def BFS(node):
+            queue = collections.deque([(root, 0, 0)])
+            while queue:
+                node, row, col = queue.popleft()
+                if node:
+                    node_list.append((col, row, node.val))
+                    queue.append((node.left, row + 1, col - 1))
+                    queue.append((node.right, row + 1, col + 1))
+
+        BFS(root)
+        node_list.sort()
+        ret = OrderedDict()
+
+        for col, row, val in node_list:
+            if col in ret:
+                ret[col].append(val)
+            else:
+                ret[col] = [val]
+
+        return ret.values()
