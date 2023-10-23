b8b062d2abd10abd471dfcd10e55f93909badcea
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-22 19:51:35 -0700

    Update Array.py

--- Facebook/Array.py
+++ Facebook/Array.py
@@ -36,3 +36,29 @@ class Find_Peak_Element:
                 if nums[i - 1] < nums[i] and nums[i + 1] < nums[i]:
                     return i
         return -1
+
+# 523. Continuous Subarray Sum
+# Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+#
+# A good subarray is a subarray where:
+#
+# its length is at least two, and
+# the sum of the elements of the subarray is a multiple of k.
+# Note that:
+#
+# A subarray is a contiguous part of the array.
+# An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+class Continuous_Subarray_Sum:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        remainder = {0: -1}
+        total = 0
+
+        for i, num in enumerate(nums):
+            total += num
+            r = total % k
+            if r not in remainder:
+                remainder[r] = i
+            elif i - remainder[r] > 1:
+                return True
+
+        return False
