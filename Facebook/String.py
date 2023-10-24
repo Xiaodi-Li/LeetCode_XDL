a7833712f7949a74f3809fc1e0630e7e29a0ad02
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-23 17:56:42 -0700

    Update String.py

--- Facebook/String.py
+++ Facebook/String.py
@@ -69,3 +69,24 @@ class Group_Shifted_Strings:
             hashmap[key] = hashmap.get(key, []) + [s]
 
         return list(hashmap.values())
+
+# 921. Minimum Add to Make Parentheses Valid
+# A parentheses string is valid if and only if:
+#
+# It is the empty string,
+# It can be written as AB (A concatenated with B), where A and B are valid strings, or
+# It can be written as (A), where A is a valid string.
+# You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+#
+# For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+# Return the minimum number of moves required to make s valid.
+class Minimum_Add:
+    def minAddToMakeValid(self, s: str) -> int:
+        ans = bal = 0
+        for ch in s:
+            bal += 1 if ch == '(' else -1
+            if bal == -1:
+                ans += 1
+                bal += 1
+        return ans + bal
+
