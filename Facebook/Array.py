290174947763de97d7c70b0a89e195b4c37f00da
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-24 16:26:57 -0700

    Update Array.py

--- Facebook/Array.py
+++ Facebook/Array.py
@@ -86,3 +86,30 @@ class Merge_Sorted_Array:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
+
+# 986. Interval List Intersections
+# You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+#
+# Return the intersection of these two interval lists.
+#
+# A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+#
+# The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+class Interval_List_Intersections:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        ans = []
+        i = j = 0
+
+        while i < len(firstList) and j < len(secondList):
+            lo = max(firstList[i][0], secondList[j][0])
+            hi = min(firstList[i][1], secondList[j][1])
+
+            if lo <= hi:
+                ans.append([lo, hi])
+
+            if firstList[i][1] <= secondList[j][1]:
+                i += 1
+            else:
+                j += 1
+
+        return ans
