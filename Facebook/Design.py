3a70c6c16abcabcc90b5bc17e165704d81ad2ead
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-24 17:27:30 -0700

    Update Design.py

--- Facebook/Design.py
+++ Facebook/Design.py
@@ -79,3 +79,30 @@ class TicTacToe:
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
 # param_1 = obj.move(row,col,player)
+
+# 346. Moving Average from Data Stream
+# Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+#
+# Implement the MovingAverage class:
+#
+# MovingAverage(int size) Initializes the object with the size of the window size.
+# double next(int val) Returns the moving average of the last size values of the stream.
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.window = []
+        self.size = size
+
+    def next(self, val: int) -> float:
+        if len(self.window) < self.size:
+            self.window.append(val)
+            return sum(self.window) / len(self.window)
+        else:
+            self.window.pop(0)
+            self.window.append(val)
+            return sum(self.window) / self.size
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
