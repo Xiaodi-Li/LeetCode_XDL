7f21c9b63a97f6c463889d513872c38df81792bd
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-10-28 11:34:42 -0700

    Update Tree.py

--- Facebook/Tree.py
+++ Facebook/Tree.py
@@ -148,3 +148,45 @@ class Diameter_of_Binary_Tree:
 
         longest_path(root)
         return diameter
+
+# 173. Binary Search Tree Iterator
+# Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+#
+# BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+# boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+# int next() Moves the pointer to the right, then returns the number at the pointer.
+# Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+#
+# You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.traverse = []
+
+        def dfs(node):
+            if node is None:
+                return
+            dfs(node.left)
+            self.traverse.append(node)
+            dfs(node.right)
+
+        dfs(root)
+        self.index = -1
+
+    def next(self) -> int:
+        self.index += 1
+        return self.traverse[self.index].val
+
+    def hasNext(self) -> bool:
+        return self.index + 1 < len(self.traverse)
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
