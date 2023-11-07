557fb0d8e151f9c20d3663efc2145d6f0883b911
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-11-07 12:10:25 -0800

    Update String.py

--- Facebook/String.py
+++ Facebook/String.py
@@ -233,3 +233,25 @@ class Count_and_Say:
             prev = new
 
         return prev
+
+# 1209. Remove All Adjacent Duplicates in String II
+# You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+#
+# We repeatedly make k duplicate removals on s until we no longer can.
+#
+# Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+class Remove_All_Adjacent_Duplicates_in_String_II:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+            else:
+                stack.append([c, 1])
+            if stack[-1][1] == k:
+                stack.pop()
+        res = ""
+        for char, count in stack:
+            res += (char * count)
+        return res
