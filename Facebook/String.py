541e45261c623269c0138404e93edd72dd3d710c
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-11-06 20:26:51 -0800

    Update String.py

--- Facebook/String.py
+++ Facebook/String.py
@@ -207,3 +207,29 @@ res:   0, 0, 0, 0, 0, 0
 right: 0, 1, 0, 2, 1, 0, 2  
 res:   0, 1, 1, 1, 1, 1, 1
 '''
+
+# 38. Count and Say
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+#
+# countAndSay(1) = "1"
+# countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+# To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+#
+# For example, the saying and conversion for digit string "3322251":
+class Count_and_Say:
+    def countAndSay(self, n: int) -> str:
+        prev = '1'
+
+        for _ in range(n - 1):
+            new = ''
+            count = 1
+            for i in range(1, len(prev)):
+                if prev[i] == prev[i - 1]:
+                    count += 1
+                else:
+                    new += str(count) + prev[i - 1]
+                    count = 1
+            new += str(count) + prev[-1]
+            prev = new
+
+        return prev
