4e26ffce3a9a393562d76c56bae01294cda1f696
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-11-08 16:36:54 -0800

    Update String.py

--- Facebook/String.py
+++ Facebook/String.py
@@ -278,3 +278,24 @@ k = 3
 missing_count: 0, 1, 
 k:             3, 2, return 6
 '''
+
+# 1216. Valid Palindrome III
+# Given a string s and an integer k, return true if s is a k-palindrome.
+#
+# A string is k-palindrome if it can be transformed into a palindrome by removing at most k characters from it.
+class Valid_Palindrome_III:
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        n = len(s)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            for j in range(n + 1):
+                if not i or not j:
+                    dp[i][j] = i or j
+                elif s[i - 1] == s[n - j]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[n][n] <= k * 2
+
