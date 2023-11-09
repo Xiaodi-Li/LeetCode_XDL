30232dda4824475633b8e5d4286109c682d03c24
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-11-08 16:36:52 -0800

    Update Sliding_Window.py

--- Facebook/Sliding_Window.py
+++ Facebook/Sliding_Window.py
@@ -23,3 +23,18 @@ class Sliding_Window_Median:
                 res.append(median)
 
         return res
+
+# 1004. Max Consecutive Ones III
+# Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+class Max_Consecutive_Ones_III:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+
+        for right in range(len(nums)):
+            k -= 1 - nums[right]
+
+            if k < 0:
+                k += 1 - nums[left]
+                left += 1
+
+        return right - left + 1
