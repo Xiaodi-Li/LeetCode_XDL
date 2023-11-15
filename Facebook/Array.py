b5557943e92c0f7a21ec4440a3f32b7e8570b716
Xiaodi Li <32185196+Xiaodi-Li@users.noreply.github.com>
2023-11-15 10:04:50 -0800

    Update Array.py

--- Facebook/Array.py
+++ Facebook/Array.py
@@ -114,3 +114,16 @@ class Interval_List_Intersections:
 
         return ans
 
+# 334. Increasing Triplet Subsequence
+# Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+class Increasing_Triplet_Subsequence:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = float('inf')
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False
